fef9a605f16344120ee0d029723d06eb237c56cb
Souvikarjun Deb <souvikarjundeb@gmail.com>
2025-06-28 01:06:28 +0530

    types defined and taskform updated

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -4,25 +4,67 @@
  * @description Assets for the app
  */
 
+'use client'
+
+
+import { useEffect,useState,useCallback } from "react"
+
 import { Textarea } from "@/components/ui/textarea"
 import { Card, CardContent, CardFooter } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
 import { Button } from "@/components/ui/button"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { Calendar } from "@/components/ui/calendar"
-import { CalendarIcon, X } from "lucide-react"
-import { Tooltip, TooltipTrigger, TooltipContent } from "@/components/ui/tooltip"
+import { CalendarIcon, X, Inbox, ChevronDown, Hash, SendHorizonal } from "lucide-react"
+import { Tooltip, TooltipTrigger } from "@/components/ui/tooltip"
 import { Command, CommandInput, CommandEmpty, CommandGroup, CommandList, CommandItem } from "@/components/ui/command"
 import { ScrollArea } from "@/components/ui/scroll-area"
+import type { ClassValue } from "clsx"
+import type { TaskForm } from "@/types"
 
+type taskFormProps ={
+  defaultFormData?: TaskForm
+  className?: ClassValue
+  mode: 'create' | 'edit'
+  onCancle?: () => void
+  onSubmit?: (formData: TaskForm) => void
+}
 
-const TaskForm = () => {
+const DEFAULT_FORM_DATA: TaskForm = {
+  content: '',
+  due_date: null,
+  project: null,
+} 
+
+const TaskForm: React.FC<taskFormProps> = ({
+  defaultFormData,
+  className,
+  mode,
+  onCancle,
+  onSubmit
+}) => {
+
+  const [ taskContent, settaskContent ] = useState(defaultFormData?.content);
+  const [ duedate, setduedate] = useState(defaultFormData?.due_date);
+  const [ projectId, setprojectId ] = useState(defaultFormData?.project);
+  const [ projectName, setprojectName ] = useState('');
+  const [ projectColorhex, setprojectColorhex ] = useState('');
+  const [ dueDateOpen, setdueDateOpen ] = useState(false);
+  const [ projectOpen, setprojectOpen ] = useState(false);
+  const [ formData, setformData ] = useState(defaultFormData)
+
   return (
     <Card className="focus-within:border-foreground/30">
         <CardContent className="p-2">
-            <Textarea className="!border-0 !ring-0 mb-2 p-2" placeholder="After finishing project Take a tour" autoFocus/>
+            <Textarea 
+              className="!border-0 !ring-0 mb-2 p-2" 
+              placeholder="After finishing project Take a tour" 
+              autoFocus
+              value={taskContent}
+              onInput={(e) => settaskContent(e.currentTarget.value)}
+            />
 
-          <div className="">
+          <div className="ring ring-border rounded-md max-w-max">
               <Popover>
                 <PopoverTrigger>
                   <Button type="button" variant="ghost" size='sm'><CalendarIcon/> Due Date</Button>
@@ -41,6 +83,53 @@ const TaskForm = () => {
               </Tooltip>
           </div>
         </CardContent>
+
+        <Separator/>
+
+        <CardFooter className="grid grid-cols-[minmax(0,1fr),max-content] gapp-2 p-2">
+          <Popover modal>
+            <PopoverTrigger asChild>
+              <Button variant='ghost' role="combobox" aria-expanded={false} className="max-w-max "><Inbox/> Inbox <ChevronDown/></Button>
+            </PopoverTrigger>
+
+            <PopoverContent className="w-[240px] p-0" align="start">
+              <Command>
+                <CommandInput placeholder="Search Project..."/>
+                <CommandList>
+                  <ScrollArea>
+                    <CommandEmpty>No Project Found</CommandEmpty>
+                    <CommandGroup>
+                      <CommandItem>
+                        <Hash/> Project 1
+                      </CommandItem>
+                      <CommandItem>
+                        <Hash/> Project 2
+                      </CommandItem>
+                      <CommandItem>
+                        <Hash/> Project 3
+                      </CommandItem>
+                      <CommandItem>
+                        <Hash/> Project 4
+                      </CommandItem>
+                    </CommandGroup>
+                  </ScrollArea>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+
+          <div className="flex items-center gap-2">
+            <Button variant='secondary'>
+              <span className="max-md:hidden">Cancle</span>
+              <X className="md:hidden"/>
+            </Button>
+            
+            <Button>
+              <span className="max-md:hidden">Add Task</span>
+              <SendHorizonal className="md:hidden"/>
+            </Button>
+          </div>
+        </CardFooter>
     </Card>
   )
 }
